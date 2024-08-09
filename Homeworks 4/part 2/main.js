057191b3a954c3f2e8fbb02f31089513f048ddb3
Illia Havrylko <2801.ilga2003@gmail.com>
2024-08-09 20:33:02 +0300

    made changes

--- Homeworks 4/part 2/main.js
+++ Homeworks 4/part 2/main.js
@@ -1,10 +1,30 @@
-let input = prompt("Введіть число:");
-let num = Number(input);
+let enteredNumber = prompt("Введіть число:");
+let number = Number(enteredNumber);
 
-if (isNaN(num) || !Number.isInteger(num)) {
+if (isNaN(number) || !Number.isInteger(number) || number <= 0) {
   alert("Ви ввели невірне число");
 } else {
-	for (let i = 1; i <= num; i++) {
-		console.log(i)
-	}
+  let perfectNumbers = [];
+
+  for (let i = 1; i <= number; i++) {
+    let sumOfDivisors = 0;
+
+    // Finding all divisors of number i
+    for (let j = 1; j <= i / 2; j++) {
+      if (i % j === 0) {
+        sumOfDivisors += j;
+      }
+    }
+
+    // Checking if the number is perfect
+    if (sumOfDivisors === i) {
+      perfectNumbers.push(i);
+    }
+  }
+
+  if (perfectNumbers.length > 0) {
+    alert("Досконалі числа: " + perfectNumbers.join(", "));
+  } else {
+    alert("Досконалих чисел немає в цьому діапазоні.");
+  }
 }
